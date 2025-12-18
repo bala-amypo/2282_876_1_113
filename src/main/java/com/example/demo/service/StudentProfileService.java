d7a577a7adafd690e0f91e39dc58e4928098705b
bala-amypo <bala@amypo.in>
2025-12-18 09:50:25 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -1,1 +1,20 @@
 package com.example.demo.service
+package com.example.demo.service;
+
+import com.example.demo.entity.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentService {
+
+    Student createData(Student student);
+
+    List<Student> fetchRecord();
+
+    Optional<Student> fetchDataById(int id);
+
+    Student updateRepeatStatus(int studentId, boolean repeatOffender);
+
+    Student findByStudentIdentifier(String studentIdentifier);
+}
