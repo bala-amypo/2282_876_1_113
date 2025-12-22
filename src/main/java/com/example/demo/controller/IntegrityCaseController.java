885813f5482f194e096134d78bdc15798cea5d90
bala-amypo <bala@amypo.in>
2025-12-22 13:37:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/IntegrityCaseController.java
+++ src/main/java/com/example/demo/controller/IntegrityCaseController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ApiResponse;
-import  com.example.demo.entity.RepeatOffenderRecord;
 import com.example.demo.entity.IntegrityCase;
 import com.example.demo.service.IntegrityCaseService;
 import org.springframework.http.ResponseEntity;
